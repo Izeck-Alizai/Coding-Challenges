726dcd5bdf83e4adbe1ff68810e9fb73e73195e5
Ahmed Ishaq Alizai <ahmedishaqalizai@gmail.com>
2022-12-21 04:38:54 +0500

    Uploaded two new Beginner challenges

---
+++ Beginner Challenges/Swap Values - Three Variables/Source.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+    int var1 = 5, var2 = 10, temp;
+
+    cout << "Var1 Before: " << var1;
+    cout << endl;
+    cout << "Var2 Before: " << var2;
+
+    cout << endl << endl;
+
+    temp = var1;
+    var1 = var2;
+    var2 = temp;
+
+    cout << "Var1 After: " << var1 << endl;
+    cout << "Var2 After: " << var2 <<endl;
+}
